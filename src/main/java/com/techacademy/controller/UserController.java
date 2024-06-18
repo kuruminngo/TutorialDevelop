bdd5479443837a03ba82284e6ee2a219cf72bcb0
kruminngo <ad@localhost>
2024-06-18 21:31:34 +0900

    課題１

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult; // 追加
-import org.springframework.validation.annotation.Validated; // 追加
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,7 +41,6 @@ public class UserController {
         return "user/register";
     }
 
-    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
     public String postRegister(@Validated User user, BindingResult res, Model model) {
@@ -54,21 +53,28 @@ public class UserController {
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 変更ここまで -----
 
-    /** User更新画面を表示 */
+    /** User更新画面を表示（追加部分） */
     @GetMapping("/update/{id}/")
-    public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
-        model.addAttribute("user", service.getUser(id));
-        // User更新画面に遷移
+    public String getUser(@PathVariable(value = "id", required = false) Integer id, Model model) {
+        if (id != null) {
+            // 一覧画面から遷移。Modelにはサービスから取得したUserをセットする
+            model.addAttribute("user", service.getUser(id));
+        } else {
+            // postUser()から遷移。ModelにはpostUser()から渡された引数のuserをセットする
+            model.addAttribute("user", model.getAttribute("user"));
+        }
         return "user/update";
     }
 
-    /** User更新処理 */
+    /** User更新処理 （追加部分）*/
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
-        // User登録
+    public String postUser(@PathVariable("id") Integer id, @Validated User user, BindingResult res, Model model) {
+        if (res.hasErrors()) {
+            model.addAttribute("user", user);
+            return getUser(null,model);
+        }
+        // User更新
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
